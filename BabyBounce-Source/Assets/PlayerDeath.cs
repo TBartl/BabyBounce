e24b418868102e955a1b2eba401ec42e866e01ac
TBartl <tbartl@umich.edu>
2018-03-17 14:48:55 -0400

    Skybox + spikes

---
+++ BabyBounce-Source/Assets/PlayerDeath.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(IntTransform))]
+public class PlayerDeath : MonoBehaviour {
+
+	IntTransform intTransform;
+
+	void Awake() {
+		intTransform = this.GetComponent<IntTransform>();		
+	}
+
+
+	void FixedUpdate() {
+		GameObject below = OccupantManager.S.OccupantAt(intTransform.position + IntVector3.down);
+		if (below && below.GetComponent<Spike>()) {
+			Debug.Log("DIE");
+		}
+	}
+}
