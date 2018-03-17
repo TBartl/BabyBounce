b605d07633397ab8200a249a237c34d9a85d967d
TBartl <tbartl@umich.edu>
2018-03-16 20:10:12 -0400

    Started blocks

---
+++ BabyBounce-Source/Assets/BabyBounce/Block.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+	static int blockCount;
+	public List<Sprite> randomSprites;
+
+	void Awake() {
+		blockCount += 1;
+	}
+
+	void Start() {
+		SelectRandomSprite();
+	}
+
+	void SelectRandomSprite() {
+		this.GetComponent<SpriteRenderer>().sprite = randomSprites[Random.Range(0, randomSprites.Count)];
+	}
+
+}
